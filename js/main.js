45fff98b7d1ec12e28a8098c48f302f7aa402373
Rohan Gaikwad <rohan.gaikwad@powerweave.com>
2020-09-22 13:17:51 +0530

    bugfix fontfamily name, create fontface

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ document.getElementById('file').addEventListener('change', (e) => {
         try {
             font = opentype.parse(e.target.result);
             renderFont(font);
+            readAsBase64(file, font);
             console.log(font);
         } catch (err) {
             showErrorMessage(err.toString());
@@ -21,14 +22,53 @@ document.getElementById('file').addEventListener('change', (e) => {
     reader.onerror = function (err) {
         showErrorMessage(err.toString());
     };
-
     reader.readAsArrayBuffer(file);
 }, false);
 
+function readAsBase64 (file, font) {
+    let base64reader = new FileReader();
+    base64reader.onload = function (e) {
+        generateFontFace(file, font, base64reader.result)
+    };
+    base64reader.onerror = function (err) {
+        showErrorMessage(err.toString());
+    };
+    base64reader.readAsDataURL(file);
+}
+
+function generateFontFace(file, font, base64String) {
+    base64String = base64String.split('base64,')[1];
+    let ext = file.name.split('.').pop();
+    let mimeTypes = {
+        ttf: 'font/ttf',
+        otf: 'font/otf',
+        woff: 'font/woff',
+        woff2: 'font/woff2',
+        eot: 'application/vnd.ms-fontobject',
+        svg: 'image/svg+xml'
+    }
+    
+    let fontFace = `@font-face {
+        font-family: '${font.names.fontFamily.en}';
+        src: url(data:${mimeTypes[ext]};charset=utf-8;base64,${base64String}) format('truetype');
+    }`;
+
+    document.querySelector('#fontstyle').innerHTML = fontFace;
+}
+
+function showErrorMessage(message) {
+    var el = document.getElementById('message');
+    if (!message || message.trim().length === 0) {
+        el.style.display = 'none';
+    } else {
+        el.style.display = 'block';
+    }
+    el.innerHTML = message;
+}
 
 function renderFont(font) {
 
-    root.style.setProperty('--font-name', `"${font.names.wwsFamily.en}`);
+    root.style.setProperty('--font-name', `"${font.names.fontFamily.en}`);
 
     for (let i = 0; i < font.glyphNames.names.length; i++) {
         if (font.glyphs.glyphs[i].unicode != undefined) {
